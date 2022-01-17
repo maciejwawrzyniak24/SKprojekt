8e45d83105422c0dd7db9852a92f3de0325093a9
Pszymczak00 <przemyslaw.p.szymczak@student.put.poznan.pl>
2022-01-17 21:30:44 +0100

    C# klient

---
+++ SKProjekt/SKProjekt/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SKProjekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public ObservableCollection<string> RoomList { get; set; } =  new ObservableCollection<string>();
+
+        private string roomText;
+        public string RoomText
+        {
+            get { return roomText; }
+            set
+            {
+                if (value != roomText)
+                {
+                    roomText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string currentRoom = "";
+        public string CurrentRoom
+        {
+            get { return currentRoom; }
+            set
+            {
+                if (value != currentRoom)
+                {
+                    ServerConnection.LeaveRoom(currentRoom);
+                    currentRoom = value;
+                    OnPropertyChanged();
+                    ServerConnection.EnterRoom(value);
+                    CurrentRoomFull = "Pokój : " + currentRoom;
+                }
+            }
+        }
+
+        public string CurrentRoomFull
+        {
+            get {  return "Pokój : " + currentRoom; }
+            set
+            {
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            UsernameTextBox.Text = ServerConnection.Username;
+            RoomList = new ObservableCollection<string>(ServerConnection.GetRoomList());
+            Ref();
+        }
+
+        public async Task Refresh()
+        {
+            if(CurrentRoom != "")
+            {
+                RoomText = await Task.Run(() => ServerConnection.GetRoomMessages(CurrentRoom));
+                RoomList = await Task.Run(() => new ObservableCollection<string>(ServerConnection.GetRoomList()));
+                ListaPokoi.ItemsSource = null;
+                ListaPokoi.ItemsSource = RoomList;
+            }
+        }
+
+        private void ChangeUsername(object sender, RoutedEventArgs e)
+        {
+            ServerConnection.Username = UsernameTextBox.Text;
+            MessageBox.Show("Zmieniono nazwe uzytkownika");
+        }
+
+        private async Task Ref()
+        {
+            while (true)
+            {
+                await Refresh();
+                await Task.Delay(100);
+            }
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount >= 2)
+            {
+                CurrentRoom = ((TextBlock)sender).Text;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+            ServerConnection.SendMessage(Wiadomosc.Text, CurrentRoom);
+            string temp = ServerConnection.GetRoomMessages(CurrentRoom);
+            RoomText = temp;
+            Wiadomosc.Text = "";
+        }
+
+        private void NowyPokoj(object sender, RoutedEventArgs e)
+        {
+            ServerConnection.NewRoom(NowyPokojTekst.Text);
+            CurrentRoom = NowyPokojTekst.Text;
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            ServerConnection.LeaveRoom(CurrentRoom);
+        }
+    }
+
+}
---
+++ SKProjekt/SKProjekt/ServerConnection.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SKProjekt
+{
+    class ServerConnection
+    {
+        public static int port { get; set; } = 1234;
+        public static TcpClient client { get; set; } = new TcpClient("192.168.56.103", port);
+        public static NetworkStream stream { get; set; } = client.GetStream();
+        public static string Username { get; set; } = "Default";
+
+        public static void SendToServer(string message, int size = 200)
+        {
+            Byte[] data2 = System.Text.Encoding.ASCII.GetBytes(message);
+            Byte[] data = new byte[size];
+            Array.Copy(data2, data, data2.Length);
+            stream.Write(data, 0, data.Length);
+        }
+
+        public static String ReceiveFromServer(int size = 200)
+        {
+            Byte[] data = new byte[size];
+            Int32 bytes = stream.Read(data, 0, size);
+            String responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            for(int i = 1; i < bytes; i++)
+            {
+                if(responseData[i] == '\x0')
+                {
+                    bytes = i;
+                    break;
+                }
+            }
+            responseData = responseData.Substring(0, bytes);
+            return responseData;
+        }
+
+
+        public static void SendMessage(string message, string room)
+        {
+            SendToServer("newMsg");
+            ReceiveFromServer();
+            SendToServer(room);
+            SendToServer(Username);
+            SendToServer(message);
+
+        }
+
+        public static List<string> GetRoomList()
+        {
+            SendToServer("listPok");
+            List<string> roomList = new List<string>();
+            ReceiveFromServer();
+
+            while (true)
+            {
+                string temp = ReceiveFromServer();
+                if(temp[0] == '\x0')
+                { break;  }
+                else
+                {
+                    roomList.Add(temp);
+                }
+            } 
+
+            return roomList;
+        }
+
+        public static string GetRoomMessages(string room)
+        {
+            string ret = "";
+
+            SendToServer("refresh");
+            ReceiveFromServer();
+            SendToServer(room);
+            SendToServer("0");
+
+            while (true)
+            {
+                string temp = ReceiveFromServer();
+                if (temp[0] == '\x0')
+                { break; }
+                else
+                {
+                    ret += temp;
+                }
+            }
+            ReceiveFromServer();
+
+            return ret;
+        }
+
+        public static void EnterRoom(string room)
+        {
+            SendToServer("onRoom");
+            ReceiveFromServer();
+            SendToServer(room);
+            ReceiveFromServer();
+        }
+
+        public static void LeaveRoom(string room)
+        {
+            SendToServer("clRoom");
+            ReceiveFromServer();
+            SendToServer(room);
+        }
+
+        public static void NewRoom(string room)
+        {
+            SendToServer("newRoom");
+            ReceiveFromServer();
+            SendToServer(room);
+        }
+    }
+}
